b707e68e7d9bba45d66b36ad70117cc9cc4535c7
Leonardo Campadelli <Campa@Macbook-Pro-do-Campa.local>
2021-12-05 20:32:21 -0300

    adjust props

--- src/home/funcional/capitulo 1/beer-card-functional-cap1.jsx
+++ src/home/funcional/capitulo 1/beer-card-functional-cap1.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../../beer-card-class.scss';
-const BeerCardFunctionalChapter1Component = () => {
+
+const BeerCardFunctionalChapter1Component = (props) => {
   return (
     <div className='-cards'>
       <div className='--none'>
         <div className='container-beerCards'>
-          <img className='-image' src={this.props.beer?.image_url} />
-          <span className='-title'>{this.props.beer?.name}</span>
+          <img className='-image' src={props.beer?.image_url} />
+          <span className='-title'>{props.beer?.name}</span>
         </div>
       </div>
     </div>
